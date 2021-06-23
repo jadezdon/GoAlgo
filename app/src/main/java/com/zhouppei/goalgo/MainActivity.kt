a1b879604d01ae32d2f97bde98065aad7de84025
zhouppei <zhouppei@gmail.com>
2021-06-23 21:21:32 +0200

    init project

---
+++ app/src/main/java/com/zhouppei/goalgo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zhouppei.goalgo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
